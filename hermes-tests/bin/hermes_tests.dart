c6cc011349e6aa9762a1af6204dcd4cea20720d2
WarriorsSami <sbarbutdica@gmail.com>
2023-02-11 11:20:11 +0200

    feat: init hermes project

---
+++ hermes-tests/bin/hermes_tests.dart
@@ -0,0 +1,5 @@
+import 'package:hermes_tests/hermes_tests.dart' as hermes_tests;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hermes_tests.calculate()}!');
+}
---
+++ hermes-tests/test/hermes_tests_test.dart
@@ -0,0 +1,8 @@
+import 'package:hermes_tests/hermes_tests.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
