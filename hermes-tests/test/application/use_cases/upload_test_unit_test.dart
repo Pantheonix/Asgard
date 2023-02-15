51a779d498f9feb9ff17a242d9acee77e61daadb
WarriorsSami <sbarbutdica@gmail.com>
2023-02-15 12:12:55 +0200

    test(upload): write unit tests for upload use case

---
+++ hermes-tests/test/application/use_cases/upload_test_unit_test.dart
@@ -0,0 +1,82 @@
+import 'package:cqrs_mediator/cqrs_mediator.dart';
+import 'package:dartz/dartz.dart';
+import 'package:hermes_tests/application/use_cases/upload_test_use_case.dart';
+import 'package:hermes_tests/di/config.dart';
+import 'package:hermes_tests/domain/entities/test_metadata.dart';
+import 'package:hermes_tests/domain/exceptions/storage_failures.dart';
+import 'package:hermes_tests/domain/interfaces/i_test_repository.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+class MockTestRepository extends Mock implements ITestRepository {}
+
+class FakeTestMetadata extends Fake implements TestMetadata {}
+
+void main() {
+  late final Map testConfig;
+  late final MockTestRepository mockTestRepository;
+  late final Mediator sut;
+
+  group('Upload Test UseCase Unit Tests', () {
+    setUpAll(() {
+      testConfig = Config.fromJsonFile('config.json').test;
+      mockTestRepository = MockTestRepository();
+
+      registerFallbackValue(FakeTestMetadata());
+      when(
+        () => mockTestRepository.upload(any()),
+      ).thenAnswer(
+        (_) async => print('test uploaded'),
+      );
+
+      Mediator.instance.registerHandler(
+        () => UploadTestAsyncQueryHandler(mockTestRepository),
+      );
+
+      sut = Mediator.instance;
+    });
+
+    test(
+        'Given metadata for test existing on disk, '
+        'When upload test use case is called, '
+        'Then no storage failure is returned', () async {
+      // Arrange
+      final TestMetadata testMetadata = TestMetadata(
+        problemId: 'marsx',
+        testId: '1',
+        srcTestRootFolder: testConfig['tempTestLocalPath'],
+        destTestRootFolder: testConfig['tempTestRemotePath'],
+      );
+
+      // Act
+      final Either<StorageFailure, Unit> result = await sut.run(
+        UploadTestAsyncQuery(testMetadata: testMetadata),
+      );
+
+      // Assert
+      expect(result.isRight(), true);
+    });
+
+    test(
+        'Given metadata for test not existing on disk, '
+        'When upload test use case is called, '
+        'Then storage failure is returned', () async {
+      // Arrange
+      final TestMetadata testMetadata = TestMetadata(
+        problemId: 'marsx',
+        testId: '3',
+        srcTestRootFolder: testConfig['tempTestLocalPath'],
+        destTestRootFolder: testConfig['tempTestRemotePath'],
+      );
+
+      // Act
+      final Either<StorageFailure, Unit> result = await sut.run(
+        UploadTestAsyncQuery(testMetadata: testMetadata),
+      );
+
+      // Assert
+      expect(result.isLeft(), true);
+      expect(result.fold(id, id), isA<StorageFailure>());
+    });
+  });
+}
