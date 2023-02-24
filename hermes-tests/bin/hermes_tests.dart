646aaf1dd2b474f67b38a9bce53c71f18cf0572f
WarriorsSami <sbarbutdica@gmail.com>
2023-02-24 13:52:53 +0200

    feat(server): add exception handling to bin server

--- hermes-tests/bin/hermes_tests.dart
+++ hermes-tests/bin/hermes_tests.dart
@@ -6,24 +6,37 @@ import 'package:hermes_tests/di/config/server_config.dart';
 import 'package:hermes_tests/di/injection.dart';
 import 'package:logger/logger.dart';
 
+// TODO 1: add better test organization in test folder in order to easily distinguish between UseCase-InputTests relationships
+// TODO 2: refactor TestMetadata to reflect every possible DTO stage (stream, archive, unarchive, remote)
+// TODO 3: declare a static helper class for file management and centralize all file operations in it
+// TODO 4: add chunking logic in FragmentTestUseCase tweaked by a given chunk size
+// TODO 5: check file content, not just metadata in integration tests
+
 Future<void> main(List<String> arguments) async {
   FirebaseDart.setup();
-  await configureDependencies('dev');
+  late final HermesGrpcServer hermesServer;
 
-  final config = getIt<ServerConfig>();
-  final logger = getIt<Logger>();
+  try {
+    await configureDependencies('dev');
 
-  final hermesServer = HermesGrpcServer(
-    config,
-    mediator,
-    logger,
-  );
-  await hermesServer.start();
-  print('Server listening on port ${config.port}...');
+    final config = getIt<ServerConfig>();
+    final logger = getIt<Logger>();
 
-  ProcessSignal.sigint.watch().listen((signal) async {
-    print('Exiting...');
-    await hermesServer.close().then((_) => print('Server closed'));
-    exit(0);
-  });
+    hermesServer = HermesGrpcServer(
+      config,
+      mediator,
+      logger,
+    );
+    await hermesServer.start();
+  } catch (e) {
+    print(e);
+  } finally {
+    ProcessSignal.sigint.watch().listen((ProcessSignal signal) async {
+      print("Exiting...");
+      await hermesServer.close().then(
+            (_) => print("Server shutdown."),
+          );
+      exit(0);
+    });
+  }
 }
