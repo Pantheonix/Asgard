c23cb088155374f090cc32ad440fc73c10dab83d
WarriorsSami <sbarbutdica@gmail.com>
2023-03-08 12:10:14 +0200

    feat(domain): add user entity and jwt config

--- quetzalcoatl-auth/Domain/Configs/JwtConfig.cs
+++ quetzalcoatl-auth/Domain/Configs/JwtConfig.cs
@@ -1,4 +1,5 @@
 public class JwtConfig
 {
     public string SecretKey { get; set; } = String.Empty;
+    public int JwtLifetime { get; set; }
 }
---
+++ quetzalcoatl-auth/Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,3 @@
+namespace Domain.Entities;
+
+public class ApplicationUser : IdentityUser { }
