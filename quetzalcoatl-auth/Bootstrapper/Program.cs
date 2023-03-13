b8f0d46adfa4ad19c96f227d773b29e6e631fc6b
WarriorsSami <sbarbutdica@gmail.com>
2023-03-13 12:02:39 +0200

    feat(di): add default swagger support

--- quetzalcoatl-auth/Bootstrapper/Program.cs
+++ quetzalcoatl-auth/Bootstrapper/Program.cs
@@ -1,30 +1,39 @@
-var builder = WebApplication.CreateBuilder(args);
+using FastEndpoints.Swagger;
 
-builder.Services.AddDbContext<AppDbContext>(
-    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
-builder.Services
-    .AddIdentity<ApplicationUser, IdentityRole>()
-    .AddEntityFrameworkStores<AppDbContext>();
-builder.Services.AddFastEndpoints(options =>
-{
-    options.DisableAutoDiscovery = true;
-    options.Assemblies = new[] { typeof(IApiMarker).Assembly, typeof(IApplicationMarker).Assembly };
-});
+var builder = WebApplication.CreateBuilder(args);
 
 var jwtConfig = new JwtConfig();
 builder.Configuration.Bind(nameof(jwtConfig), jwtConfig);
 
-builder.Services.AddSingleton(jwtConfig);
-builder.Services.AddJWTBearerAuth(jwtConfig.SecretKey);
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
-builder.Services.AddAutoMapper(typeof(IApiMarker), typeof(IApplicationMarker));
+builder.Services
+    .AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services
+    .AddFastEndpoints(options =>
+    {
+        options.DisableAutoDiscovery = true;
+        options.Assemblies = new[]
+        {
+            typeof(IApiMarker).Assembly,
+            typeof(IApplicationMarker).Assembly
+        };
+    })
+    .AddSingleton(jwtConfig)
+    .AddJWTBearerAuth(jwtConfig.SecretKey)
+    .AddAutoMapper(typeof(IApiMarker), typeof(IApplicationMarker))
+    .AddSwaggerDoc();
 
 var app = builder.Build();
 
-app.UseDefaultExceptionHandler();
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseFastEndpoints();
+app.UseDefaultExceptionHandler()
+    .UseAuthentication()
+    .UseAuthorization()
+    .UseFastEndpoints()
+    .UseSwaggerGen();
 
 app.Run();
