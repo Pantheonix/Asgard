dd6b436cef478870b883039d51e504b71472b855
Bărbuț-Dică Sami <70215066+WarriorsSami@users.noreply.github.com>
2023-07-17 13:13:33 +0000

    feat(problem): implement GetList endpoint

--- enki-problems/src/EnkiProblems.Application/Problems/ProblemAppService.cs
+++ enki-problems/src/EnkiProblems.Application/Problems/ProblemAppService.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using Volo.Abp.Authorization;
 using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Application.Dtos;
+using Microsoft.AspNetCore.Authorization;
 
 namespace EnkiProblems.Problems;
 
@@ -20,6 +23,7 @@ public class ProblemAppService : EnkiProblemsAppService, IProblemAppService
         _problemRepository = problemRepository;
     }
 
+    [Authorize]
     public async Task<ProblemDto> CreateAsync(CreateProblemDto input)
     {
         if (CurrentUser.Roles.All(r => r != EnkiProblemsConsts.ProposerRoleName))
@@ -48,5 +52,44 @@ public class ProblemAppService : EnkiProblemsAppService, IProblemAppService
         await _problemRepository.InsertAsync(problem);
 
         return ObjectMapper.Map<Problem, ProblemDto>(problem);
+    }
+
+    public async Task<PagedResultDto<ProblemDto>> GetListAsync(ProblemListFilterDto input)
+    {
+        var problemQueryable = await _problemRepository.GetQueryableAsync();
+
+        problemQueryable = problemQueryable.Where(p => p.IsPublished);
+
+        if (!string.IsNullOrEmpty(input.Name))
+        {
+            problemQueryable = problemQueryable.Where(p => p.Name.Contains(input.Name));
+        }
+
+        if (input.IoType != null)
+        {
+            problemQueryable = problemQueryable.Where(p => p.IoType == input.IoType);
+        }
+
+        if (input.Difficulty != null)
+        {
+            problemQueryable = problemQueryable.Where(p => p.Difficulty == input.Difficulty);
+        }
+
+        if (input.ProgrammingLanguages != null)
+        {
+            problemQueryable = problemQueryable.Where(
+                p => p.ProgrammingLanguages.Any(l => input.ProgrammingLanguages.Contains(l))
+            );
+        }
+
+        problemQueryable = problemQueryable.PageBy(input).OrderBy(p => p.CreationDate);
+
+        var totalCount = await AsyncExecuter.CountAsync(problemQueryable);
+        var problems = await AsyncExecuter.ToListAsync(problemQueryable);
+
+        return new PagedResultDto<ProblemDto>(
+            totalCount,
+            ObjectMapper.Map<List<Problem>, List<ProblemDto>>(problems)
+        );
     }
 }
